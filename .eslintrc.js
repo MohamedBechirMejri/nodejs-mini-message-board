49b33dfe50491b52d7b6691fe5bf77c957909ec9
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-06-19 02:34:25 +0100

    init app

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "no-console": "warn",
+    "no-unused-vars": "warn",
+    "no-underscore-dangle": "off",
+  },
+};
